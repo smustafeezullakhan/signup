46be4104b83611f36e45b876e13822adcd99372c
Khan <mustafeez72777@gmail.com>
2023-07-20 17:32:29 +0530

    home page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <nav class="nav nav-pills nav-fill">
+  <a class="nav-link active" aria-current="page" href="#">Active</a>
+  <a class="nav-link" href="#">Much longer nav link</a>
+  <a class="nav-link" href="#">Link</a>
+  <a class="nav-link disabled">Disabled</a>
+</nav>
+    </div>
+  );
+}
+
+export default App;
